46177887f1da6c7b77dfbb4094131bb8739a6e8b
DominicDevlin <dmncdevlin@gmail.com>
2024-12-17 00:32:12 +0900

    .

--- model-output/plot-line.py
+++ model-output/plot-line.py
@@ -4,19 +4,38 @@ import matplotlib.pyplot as plt
 # Load the data from a text file
 # The data.txt file should contain the provided rows of numbers
 dataphi = np.loadtxt('phi_data.dat')
+
+datarho = np.loadtxt('rho_data.dat')
+
+datadiff = np.loadtxt('diff_data.dat')
+
 dataphi = dataphi[dataphi[:, 1].argsort()]
+datarho = datarho[datarho[:, 1].argsort()]
+datadiff = datadiff[datadiff[:, 1].argsort()]
+
 # The first column is the x-axis
 x = dataphi[:, 0]
 y = dataphi[:, 1]   # y-values in the second column
 phi = dataphi[:, 2] # concentration in the third column
+rho = datarho[:, 2] # concentration in the third column
+diff = datadiff[:, 2]
+
+
 
 y_values=[]
 phi_values=[]
+rho_values=[]
+diff_values = []
 
 for i in range(len(x)):
     if x[i] < 0.01 and x[i] > -0.01:
         y_values.append(y[i])
         phi_values.append(phi[i])
+        rho_values.append(rho[i])
+        if diff[i] < 0.01:
+            diff_values.append(0)
+        else:
+            diff_values.append(diff[i])
 
 
 
@@ -26,6 +45,14 @@ fig, ax1 = plt.subplots()
 
 # Plot the first two lines on the primary y-axis
 ax1.plot(y_values, phi_values, linestyle='-', color='black', label='yphi')
+ax1.plot(y_values, rho_values, linestyle='-', color='red', label='yrho')
+
+ax2 = ax1.twinx()
+# Plot the third line on the secondary y-axis
+ax2.plot(y_values, diff_values, linestyle='-', color='blue', label='ydiff')
+ax2.set_ylabel('Secondary Y-axis Label')
+
+
 ax1.set_xlabel('X-axis Label')
 ax1.set_ylabel('Primary Y-axis Label')
 
