a851ef46bd16ca0b135a96e6c0de8c49871603af
DominicDevlin <dmncdevlin@gmail.com>
2025-01-07 10:32:12 +1300

    .

--- oned-pyscript.py
+++ oned-pyscript.py
@@ -1,10 +1,11 @@
 import subprocess
 
 # Define the range for the two parameters, e.g., 1 to 10.
-start, end = 1, 20
+startphi, endphi = 1, 1
+statrho, endrho = 1, 40
 
-for i in range(1, 2):
-    for j in range(start, end + 1):
+for i in range(startphi, endphi+1):
+    for j in range(statrho, endrho + 1):
         # Build the command as a list of strings
         command = ["FreeFem++", "oned-working.edp", str(i), str(j)]
         
