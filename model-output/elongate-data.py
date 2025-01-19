1ca394bd8624e35e8a37fe3cb32eb2ae85faf5f2
DominicDevlin <dmncdevlin@gmail.com>
2025-01-19 23:29:30 +1300

    .

--- model-output/elongate-data.py
+++ model-output/elongate-data.py
@@ -6,18 +6,17 @@ import matplotlib.pyplot as plt
 # The data.txt file should contain the provided rows of numbers
 # datan= '-2-42.2-1.72'
 # Get a list of all subdirectories in the 'data' directory
-data_dir = 'data/bigdd/'
+data_dir = 'data/'
 subdirectories = [subdir for subdir in os.listdir(data_dir) if os.path.isdir(os.path.join(data_dir, subdir))]
 
 # constant params.
 tauphi = 2.
 
 eps=0.001
-sigma12s = [0.12, 0.18, 0.24, 0.3, 0.36] # <-- desired sigma_12 value
 
-data_taus = []
-data_tau_rhophis = []
 y_thresholds = []
+tau_strings = []
+
 
 # Iterate through each subdirectory
 for subdir in subdirectories:
@@ -28,7 +27,10 @@ for subdir in subdirectories:
     tphi = float(datan_numbers[0])
     trhophi = float(datan_numbers[1])
     trho = float(datan_numbers[2])
+    ttt = [tphi, trhophi, trho]
+
     
+
 
     # Load the data from the current subdirectory
     if not os.path.exists(dir + 'phi_data' + datan + '.dat') or not os.path.exists(dir + 'rho_data' + datan + '.dat') or not os.path.exists(dir + 'diff_data' + datan + '.dat'):
@@ -41,6 +43,7 @@ for subdir in subdirectories:
     dataphi = dataphi[dataphi[:, 1].argsort()]
     datarho = datarho[datarho[:, 1].argsort()]
     datadiff = datadiff[datadiff[:, 1].argsort()]
+    
 
     # The first column is the x-axis
     x = dataphi[:, 0]
@@ -58,18 +61,17 @@ for subdir in subdirectories:
         if x[i] < 0.05 and x[i] > -0.05:
             y_values.append(y[i])
             phi_values.append(phi[i])
-            rho_values.append(rho[i])
-            if diff[i] < 0.01:
-                diff_values.append(np.nan)
-            else:
-                diff_values.append(diff[i])
+            # if diff[i] < 0.01:
+            #     diff_values.append(np.nan)
+            # else:
+            #     diff_values.append(diff[i])
 
-    # Find the y value for which phi_value goes below 0.5
-    threshold = 0.8
+    # Find the y value for which phi_value goes below 0.x
+    threshold = 0.5
     y_threshold = None
 
     for y_val, phi_val in zip(y_values, phi_values):
-        if phi_val < threshold and y_val > 1:
+        if phi_val < threshold and y_val > 0.5:
             y_threshold = y_val
             break
     
@@ -80,108 +82,65 @@ for subdir in subdirectories:
     if y_threshold < 0:
         y_threshold = 0
     
-    data_taus.append((trhophi, trho))
-    data_tau_rhophis.append((float(trhophi)))
+    tau_strings.append(ttt)
     y_thresholds.append(y_threshold)
     # print(trhophi, trho, y_threshold)
-
-    # Print the y_threshold for the current subdirectory
-    # print(f"y_threshold for {trhophi, trho}: {y_threshold}")
-
 
 
 
 plot_x_values=[]
 plot_y_values=[]
 
-for sigma12_target in sigma12s:
-    # tauphirho_values = [1, 1.5625, 2.25, 3.0625, 4, 5.0625, 6.25, 7.5625, 9, 12.25, 16, 20.25, 25, 30.25, 36, 42.25, 49]
-    # tauphirho_values = [1, 1.5625, 2.25, 3.0625, 4, 5.0625, 6.25, 7.5625, 9, 10.5625, 12.25, 14.0625, 16, 18.0625, 20.25, 22.5625, 25, 27.5625, 30.25, 33.0625, 36, 39.0625, 42.25, 45.5625, 49]
-    tauphirho_values = [1, 1.5625, 2.25, 3.0625, 4, 5.0625, 6.25, 7.5625, 9, 10.5625, 12.25, 14.0625, 16, 18.0625, 20.25, 22.5625, 25, 27.5625, 30.25, 33.0625, 36, 39.0625, 42.25, 45.5625, 49, 52.5625, 56.25, 60.0625, 64, 68.0625, 72.25, 76.5625, 81, 85.5625, 90.25, 95.0625, 100]
+tauphirho_values = [4, 9, 16, 25, 36, 49]
 
-    taurho_values = []
-    sigma13_values = []
-    real_sigma13s = []
+for tpr in tauphirho_values:
 
-    for tauphirho in tauphirho_values:
-        # ---- 1) Compute quantities that depend on tauphirho but not on taurho ----
-        #
-        # phiint = [ (32/9)*tauphi*eps + (4/9)*tauphirho*eps ] / [ (8/9)*tauphirho*eps ]
-        # Simplifies to phiint = 4*(tauphi/tauphirho) + 1/2
-
-        # gamma_phirho1 = sqrt( (8/9)*tauphirho*eps * phiint )
-        gamma_phirho1 = np.sqrt((8./9.)*tauphirho*eps)
-
-        # ---- 2) Solve for taurho so that sigma12 stays fixed ----
-        # We want sigma12_target = gamma_phirho1 + sqrt((8/9)*taurho*eps).
-        # => taurho = ( (sigma12_target - gamma_phirho1)**2 ) / ( (8/9)*eps )
-        const_for_taurho = (8./9.) * eps
-        # Be careful to avoid negative radicand:
-        inside_sqrt = sigma12_target - gamma_phirho1
-        
-        if inside_sqrt < 0:
-            # If gamma_phirho1 alone is already bigger than sigma12_target, 
-            # then you cannot keep sigma_12 at that smaller value.
-            taurho = None
-            sigma12_computed = None
-        else:
-            taurho = (inside_sqrt**2) / const_for_taurho
-
-            # ---- 3) Re-compute gamma_rho with the solved taurho ----
-            gamma_rho = np.sqrt((8./9.)*taurho*eps)
-
-            # Double check sigma12 is indeed ~ sigma12_target
-            sigma12_computed = gamma_phirho1 + gamma_rho
-
-        # ---- 4) Now we can compute sigma13, sigma23, etc. if needed ----
-        # gamma_phi  = sqrt((8/9)*tauphi*eps)
-        gamma_phi = np.sqrt((8./9.)*tauphi*eps)
-        
-        # gamma_phirho2 = sqrt((4/9)*tauphirho*eps)
-        gamma_phirho2 = np.sqrt((4./9.)*tauphirho*eps)
-
-        # sigma13 = gamma_phirho2 + gamma_rho + gamma_phi
-        # sigma23 = gamma_phi
-        sigma13 = None
-        sigma23 = None
-        
-        if taurho is not None:
-            sigma13 = gamma_phirho2 + gamma_rho + gamma_phi
-            # newsig12 = gamma_phirho1 = np.sqrt((8./9.)*tauphirho*eps) + gamma_rho
-            sigma13_values.append(sigma13/ sigma12_target)
-            sigma23 = gamma_phi
-            taurho_values.append(taurho)
-
-    constant_list =[]
-    sigma13_shorts = []
-    for r in range(len(taurho_values)):
-        tpr = tauphirho_values[r]
-        tr = taurho_values[r]
-        constant_list.append((tpr, tr))
-        sigma13_shorts.append(sigma13_values[r])
-    # print(constant_list, sigma13_shorts)
-
+    indices = []
+    
     plotx=[]
     ploty=[]
-    for ir in range(len(data_taus)):
-        for iter in range(len(constant_list)):
-            # print(str(data_tau), constant_tuple)
-            if (abs(data_taus[ir][0] - constant_list[iter][0]) < 0.49 and abs(data_taus[ir][1] - constant_list[iter][1]) < 0.49):
-                y_threshold = y_thresholds[ir]
-                plotx.append(sigma13_shorts[iter])
-                ploty.append(y_threshold)
-                # print(sigma13_shorts[iter], y_threshold)
-                # print(f"y_threshold for {data_taus[ir]}: {y_threshold}")
-                break
+    
+    for i in range(len(tau_strings)):
+        if tau_strings[i][1] == tpr:
+            indices.append(i)
+    
             
+    for i in range(len(indices)):
+
+
+        tphi = tau_strings[indices[i]][0]
+        tphirho = tau_strings[indices[i]][1]
+        trho = tau_strings[indices[i]][2]
+        const1 = (5*np.sqrt(2)/12.) * eps
+        const2 = (5*np.sqrt(2)/12.) * eps * 0.5
+        # Be careful to avoid negative radicand:
+        gammaphi = (const1 * tphi)
+        gammaphirho = const2 * tphirho
+        gammarho = (const2 * trho)
+
+        sigmaHM = np.sqrt(np.sqrt(gammaphi) * np.sqrt(gammarho) + gammarho )
+        sigmaHL = np.sqrt(gammaphirho)
+        sigmaLM = 0
+
+        sigratio = sigmaHM/sigmaHL
+        yval = y_thresholds[indices[i]]
+        plotx.append(sigratio)
+        ploty.append(yval)
+        
+        print(trho, tphirho, yval)
+
     # Sort the plot_x_values and plot_y_values based on plot_x_values
     sorted_data = sorted(zip(plotx, ploty), key=lambda x: x[0])
 
     # Unzip the sorted data
     sorted_plot_x_values, sorted_plot_y_values = zip(*sorted_data)
+    print(sorted_plot_x_values, sorted_plot_y_values)
     
     plot_x_values.append(sorted_plot_x_values)
     plot_y_values.append(sorted_plot_y_values)
+
+
+
 
 # Make matplotlib line plot
 colors = ['red', 'blue', 'green', 'orange', 'purple']
@@ -197,7 +156,7 @@ for i in range(len(plot_x_values)):
         plot_y_values[i],
         'o-',
         color=colors[i % len(colors)],
-        label=f'sigmaHL = {sigma12s[i]}'
+        label=f'sigmaHL = {tauphirho_values[i]}'
     )
 
 # Set tick properties explicitly
