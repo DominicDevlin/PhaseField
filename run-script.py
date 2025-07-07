ba80b822131d61d9e43c516f82f891b79a43566e
DominicDevlin <dmncdevlin@gmail.com>
2025-07-07 22:42:02 +1200

    .

--- run-script.py
+++ run-script.py
@@ -3,29 +3,6 @@ import sys
 import numpy as np
 import sympy as sp
 
-def get_unknowns(gamma12, gamma13, gamma23, eps):
-    # Define unknowns
-    sig12, sig13, sig23 = sp.symbols('sig12 sig13 sig23', positive=True, real=True)
-
-
-    # Define the three equations:
-    eq1 = eps/6 * sp.sqrt(2*sig12*(4*sig12 + sig13 + sig23)) - gamma12
-    eq2 = eps/6 * sp.sqrt(2*sig13*(4*sig13 + sig12 + sig23)) - gamma13
-    eq3 = eps/6 * sp.sqrt(2*sig23*(4*sig23 + sig12 + sig13)) - gamma23
-
-    # Provide an initial guess for [sig12, sig13, sig23]:
-    initial_guess = [5.0, 5.0, 5.0]
-
-    # Solve numerically
-    solution = sp.nsolve([eq1, eq2, eq3], [sig12, sig13, sig23], initial_guess)
-
-    # Unpack and display
-    sig12_sol, sig13_sol, sig23_sol = solution
-    # print(f"sig12 = {sig12_sol:.6f}")
-    # print(f"sig13 = {sig13_sol:.6f}")
-    # print(f"sig23 = {sig23_sol:.6f}")
-    return sig12_sol, sig13_sol, sig23_sol
-
 
 index = 0
 
@@ -37,20 +14,22 @@ if (len(sys.argv) > 1):
 
 output = []
 results = []
-eps = 0.015
+eps = 0.03
 
 gamma12_values =  [0.02, 0.04, 0.06, 0.08, 0.1]
 gamma23_values = [0.02, 0.04, 0.06, 0.08, 0.1]
 
-gamma13 = 0.03
+gamma13 = 0.02
 
 
 
 for gamma12 in gamma12_values:
     for gamma23 in gamma23_values:
-        sig12, sig13, sig23 = get_unknowns(gamma12, gamma13, gamma23, eps)
+        sig12 = 3 * gamma12 / eps
+        sig13 = 3 * gamma13 / eps
+        sig23 = 3 * gamma23 / eps
         output.append((sig12, sig13, sig23))
-        # print(sig12, sig13, sig12)
+        print(sig12, sig13, sig23)
 
 sigma12, sigma13, sigma23 = output[index]
 
