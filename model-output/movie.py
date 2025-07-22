86726bab4d2bb452770465f2549cae9219787d46
DominicDevlin <dmncdevlin@gmail.com>
2025-07-22 13:37:42 +1200

    .

--- model-output/movie.py
+++ model-output/movie.py
@@ -14,7 +14,7 @@ import sys  # Used to exit the script gracefully
 
 # --- Main settings to configure ---
 # Directory containing the data files
-data_directory = 'data/highdiff/21-9-9/'
+data_directory = 'data/highdiff/18-9-18/'
 # The increment between time steps (e.g., files are t=20, t=40, etc.)
 time_step_increment = 20
 
