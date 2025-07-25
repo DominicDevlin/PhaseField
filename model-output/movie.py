a988f0cb8b87a13deca3798928335dc795b369ac
DominicDevlin <dmncdevlin@gmail.com>
2025-07-25 16:42:10 +1200

    .

--- model-output/movie.py
+++ model-output/movie.py
@@ -14,7 +14,7 @@ import sys  # Used to exit the script gracefully
 
 # --- Main settings to configure ---
 # Directory containing the data files
-data_directory = 'data/sig13low/18-9-18/'
+data_directory = 'data/visc/24-9-18/'
 # The increment between time steps (e.g., files are t=20, t=40, etc.)
 time_step_increment = 20
 
