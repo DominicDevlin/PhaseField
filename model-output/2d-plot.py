dd24b8d1ced9ae2c11ad34cc5d259ab1fe2e87d3
DominicDevlin <dmncdevlin@gmail.com>
2025-10-08 15:42:29 +1300

    .

--- model-output/2d-plot.py
+++ model-output/2d-plot.py
@@ -12,9 +12,9 @@ from matplotlib.cm import get_cmap
 # ---------------------------------------------------------------
 
 # number = '30-3-30'
-number = 'data/diff-phase/15-0.5'
+number = 'data/diff-phase/12-0.5'
 prepend = '' + number + '/'
-time = '680'
+time = '760'
 
 
 df = pd.read_csv(prepend + 'c1' + '-' + time + '.dat', header=None, names=['x', 'y', 'conc'], delimiter='\t')
